6faf33f85d61ff6f4a0bc9d98bcb29f517ce987b
tomhockenberry <tomhockenberry03@gmail.com>
2021-04-09 09:06:10 -0400

    Finished text editor

--- code.cpp
+++ code.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-cout << "Test"
-cout << "test3"
+int main(){
+    string input;
+    cout << "What do you want to put in the text file?" << endl << "Type EXIT on a new line to close" << endl;
+    ofstream file;
+    file.open("Text.txt", ios_base :: app);
+    while (input!="EXIT") {
+        getline (cin, input);
+        if (input != "EXIT") {
+            file << input << " ";
+        }
+    }
+    file.close();
+}
