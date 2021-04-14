27c08d9b444a84412677527473ae4a8d490b253b
tomhockenberry <tomhockenberry03@gmail.com>
2021-04-14 08:19:24 -0400

    Added recursive factorial calculator

---
+++ Number Sorter.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+
+    string raw; //gets input from the text file and writes it to a string of raw data
+    ifstream file;
+    file.open("list10k.txt");
+    getline (file, raw);
+    file.close();
+
+
+    int number_of_spaces = 0; // determines the number of spaces in the string of raw data
+    for (int i = 0; i < raw.length(); i++){
+      if (raw[i] == ' '){
+        number_of_spaces ++;
+      }
+    }
+
+    string s_array[number_of_spaces - 1]; // separates the raw data by spaces and forms a list of the different sections.
+    int i = 0;
+    int last_space = 0;
+    while (i < number_of_spaces - 1){
+      for (int j = last_space + 1; j < raw.length(); j++){
+        if (raw[j] == ' '){
+          s_array[i] = raw.substr(last_space, j);
+          last_space = j;
+          break;
+        }
+      }
+      i++;
+    }
+
+    int i_array[number_of_spaces - 1]; //Takes the sting array and converts it to an integer array.
+    for (int k = 0; k < number_of_spaces - 1; k++){
+      i_array[k] = stoi(s_array[k]);
+    }
+
+    int a = 1; //sorts the array
+    while(a == 1){
+      a = 0;
+      for (i = 0; i < number_of_spaces - 1; i++){
+        int first_entry = i_array[i];
+        int second_entry = i_array[i+1];
+        if(first_entry > second_entry){
+          a = 1;
+          int swap = first_entry;
+          i_array[i] = second_entry;
+          i_array[i+1] = swap;
+        }
+      }
+    }
+    for (i = 0; i < number_of_spaces - 1; i++){
+        cout << i_array[i] << ' ';
+    }
+    return 0;
+}
--- code.cpp
+++ code.cpp
@@ -1,58 +1,17 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
 
+unsigned long long Factorial(unsigned long long n) {
+  if (n == 0) {
+    return 1;
+  }
+  else {
+    return n * Factorial(n - 1);
+  }
+}
+
 int main(){
-
-    string raw; //gets input from the text file and writes it to a string of raw data
-    ifstream file;
-    file.open("list10k.txt");
-    getline (file, raw);
-    file.close();
-
-
-    int number_of_spaces = 0; // determines the number of spaces in the string of raw data
-    for (int i = 0; i < raw.length(); i++){
-      if (raw[i] == ' '){
-        number_of_spaces ++;
-      }
-    }
-
-    string s_array[number_of_spaces - 1]; // separates the raw data by spaces and forms a list of the different sections.
-    int i = 0;
-    int last_space = 0;
-    while (i < number_of_spaces - 1){
-      for (int j = last_space + 1; j < raw.length(); j++){
-        if (raw[j] == ' '){
-          s_array[i] = raw.substr(last_space, j);
-          last_space = j;
-          break;
-        }
-      }
-      i++;
-    }
-
-    int i_array[number_of_spaces - 1]; //Takes the sting array and converts it to an integer array.
-    for (int k = 0; k < number_of_spaces - 1; k++){
-      i_array[k] = stoi(s_array[k]);
-    }
-
-    int a = 1; //sorts the array
-    while(a == 1){
-      a = 0;
-      for (i = 0; i < number_of_spaces - 1; i++){
-        int first_entry = i_array[i];
-        int second_entry = i_array[i+1];
-        if(first_entry < second_entry){
-          a = 1;
-          int swap = first_entry;
-          i_array[i] = second_entry;
-          i_array[i+1] = swap;
-        }
-      }
-    }
-    for (i = 0; i < number_of_spaces - 1; i++){
-        cout << i_array[i] << ' ';
-    }
-    return 0;
+  int number;
+  cin >> number;
+  cout << Factorial(number);
 }
